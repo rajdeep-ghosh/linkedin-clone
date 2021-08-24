a103a96652d95d2f691b98a4cc1f7bc1736ed3db
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-08-24 22:22:23 +0530

    Create Header component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+
+function App() {
+  return (
+    <div>
+      {/* Header */}
+      <Header />
+      {/* App Body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+  return <div>
+      Its header
+  </div>;
+}
+
+export default Header;
