0602753fbde68de5ab9a56079e8d67fbac2d21fe
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-08-26 05:14:57 +0530

    Render header__left

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,19 @@
+import "../styles/Header.css";
+import LinkedinLogo from "../images/linkedin-logo.png";
+import SearchIcon from '@material-ui/icons/Search';
+
 function Header() {
-  return <div>Its header</div>;
+  return (
+    <header className="header">
+      <div className="header__left">
+        <img src={LinkedinLogo} loading="lazy" alt="LinkedIn Logo" />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
