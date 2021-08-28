550016740306b43a33592fb76e7e7c5cf0e68c36
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-08-28 09:13:41 +0530

    Create HeaderOption component and render elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import "../styles/Header.css";
+import HeaderOption from "./HeaderOption";
 import LinkedinLogo from "../images/linkedin-logo.png";
-import SearchIcon from '@material-ui/icons/Search';
+import { Home, Search, SupervisorAccount } from "@material-ui/icons";
 
 function Header() {
   return (
@@ -8,9 +9,13 @@ function Header() {
       <div className="header__left">
         <img src={LinkedinLogo} loading="lazy" alt="LinkedIn Logo" />
         <div className="header__search">
-          <SearchIcon />
+          <Search />
           <input type="text" placeholder="Search" />
         </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption title="Home" Icon={Home} />
+        <HeaderOption title="My Network" Icon={SupervisorAccount} />
       </div>
     </header>
   );
---
+++ src/components/HeaderOption.js
@@ -0,0 +1,12 @@
+import "../styles/HeaderOption.css";
+
+function HeaderOption({ title, Icon }) {
+  return (
+    <div className="headerOption">
+      {Icon && <Icon className="headerOption__icon" />}
+      <h3 className="headerOption__title">{title}</h3>
+    </div>
+  );
+}
+
+export default HeaderOption;
