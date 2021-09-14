743493818bc77809f9f6d55e1910f87188d14c4a
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-09-14 05:56:44 +0530

    Style feed input

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,7 +1,20 @@
 import "../styles/Feed.css";
+import { Create } from "@material-ui/icons";
 
 function Feed() {
-  return <div></div>;
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <Create />
+          <form>
+            <input type="text"></input>
+            <button type="submit">Post</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Feed;
