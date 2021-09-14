45f9f1c3039ac5aba1708fe5b26a92a4e5e85948
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-09-14 06:55:50 +0530

    Complete FeedInputOption

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,12 @@
 import "../styles/Feed.css";
-import { Create } from "@material-ui/icons";
+import { 
+  CalendarViewDay, 
+  Create, 
+  EventNote, 
+  Image, 
+  Subscriptions 
+} from "@material-ui/icons";
+import FeedInputOption from "./FeedInputOption";
 
 function Feed() {
   return (
@@ -11,6 +18,12 @@ function Feed() {
             <input type="text"></input>
             <button type="submit">Post</button>
           </form>
+        </div>
+        <div className="feed__inputOptions">
+          <FeedInputOption title="Photo" Icon={Image} iconColor="#70b5f9" />
+          <FeedInputOption title="Video" Icon={Subscriptions} iconColor="#e7a33e" />
+          <FeedInputOption title="Event" Icon={EventNote} iconColor="#fe2575" />
+          <FeedInputOption title="Write Article" Icon={CalendarViewDay} iconColor="#7fc15e" />
         </div>
       </div>
     </div>
--- src/components/FeedInputOption.js
+++ src/components/FeedInputOption.js
@@ -1,7 +1,12 @@
 import "../styles/FeedInputOption.css";
 
-function FeedInputOption() {
-  return <div></div>;
+function FeedInputOption({ title, Icon, iconColor }) {
+  return (
+    <div className="feed__inputOption">
+      <Icon style={{ color: iconColor }} />
+      <h4>{title}</h4>
+    </div>
+  );
 }
 
 export default FeedInputOption;
