4dc14b1eaa9b3b8c8ba2b1063f993bb7653b75a0
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-09-23 12:37:39 +0530

    Make login page somewhat mobile resonsive

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,18 +10,24 @@ function Login() {
         </a>
       </header>
       <div className="login__body">
-        <h1 className="login__title">Welcome to your professional community</h1>
-        <form className="login__form">
-          <input type="text" placeholder="Full Name" required />
-          <input type="url" placeholder="Profile Pic URL (optional)" />
-          <input type="email" placeholder="Email" required /> 
-          <input type="password" placeholder="Password" required />
-          <button type="submit">Sign in</button>
-        </form>
-        <p className="login__register">
-          Not a member?{" "}
-          <span>Register Now</span>
-        </p>
+        <div>
+          <h1 className="login__title">Welcome to your professional community</h1>
+          <form className="login__form">
+            <input type="text" placeholder="Full Name" required />
+            <input type="url" placeholder="Profile Pic URL (optional)" />
+            <input type="email" placeholder="Email" required /> 
+            <input type="password" placeholder="Password" required />
+            <button type="submit">Sign in</button>
+          </form>
+          <p className="login__register">
+            Not a member?{" "}
+            <span>Register Now</span>
+          </p>
+        </div>
+        <div className="login__bgImg">
+          <img className="img-d" src="https://static-exp1.licdn.com/sc/h/dxf91zhqd2z6b0bwg85ktm5s4" alt="bg-img" />
+          <img className="img-m" src="https://static-exp1.licdn.com/sc/h/d58zfe6h3ycgq5l1ccjpkrtdn" alt="bg-img" />
+        </div>
       </div>
     </div>
   );
@@ -29,4 +35,4 @@ function Login() {
 
 export default Login;
 
-// https://static-exp1.licdn.com/sc/h/dxf91zhqd2z6b0bwg85ktm5s4
+// https://static-exp1.licdn.com/sc/h/d58zfe6h3ycgq5l1ccjpkrtdn 
