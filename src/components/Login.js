3217746e49a54bb6f6c73c91e017370ae0fe7fe7
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-09-23 10:04:52 +0530

    Make login header

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,18 @@
 import "../styles/Login.css";
+import LinkedinLogoFull from "../images/LI-Logo.png";
 
 function Login() {
-  return <div className="login">Not logged in</div>;
+  return (
+    <div className="login">
+      <header className="login__header">
+        <a href="/">
+          <img src={LinkedinLogoFull} alt="Linkedin Logo" />
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default Login;
+
+// https://static-exp1.licdn.com/sc/h/dxf91zhqd2z6b0bwg85ktm5s4
