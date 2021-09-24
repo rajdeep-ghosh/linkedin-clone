d2b43d0e517965b3d5df4aae160451deeb050e10
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-09-24 12:37:11 +0530

    Add footer text

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -18,15 +18,20 @@ function Widget() {
 
   return (
     <div className="widget">
-      <div className="widget__header">
-        <h2>LinkedIn News</h2>
-        <Info />
+      <div className="widget__info">
+        <div className="widget__header">
+          <h2>LinkedIn News</h2>
+          <Info />
+        </div>
+        {news("Apple is back", "Top news - 9574 readers")}
+        {news("Covid India updates", "Top news - 645 readers")}
+        {news("India vaccinates 25 million people in a single day", "Top news - 13470 readers")}
+        {news("Tesla hits new high", "Cars & auto - 365 readers")}
+        {news("Doge to Moon", "Crypto - 123 readers")}
       </div>
-      {news("Apple is back", "Top news - 9574 readers")}
-      {news("Covid India updates", "Top news - 645 readers")}
-      {news("India vaccinates 25 million people in a single day", "Top news - 13470 readers")}
-      {news("Tesla hits new high", "Cars & auto - 365 readers")}
-      {news("Doge to Moon", "Crypto - 123 readers")}
+      <div className="widget__copyright">
+        <a href="https://github.com/rajdeep-ghosh" target="_blank" rel="noreferrer">Rajdeep Ghosh &copy; 2021</a>
+      </div>
     </div>
   );
 }
